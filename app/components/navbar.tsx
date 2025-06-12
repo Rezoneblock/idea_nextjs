74c0ed85cd3421780fce9db61bad0b4f0e996825
Гордеев Тимур <trew.tr2019@yandex.ru>
2025-06-12 17:53:47 +0300

    switched from tailwindcss 3 (using theme variables via config.ts) to 4 (@theme in globals.css)

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -6,7 +6,7 @@ import React from "react";
 const Navbar = async () => {
 	const session = await auth();
 	return (
-		<header className="px-5 py-3 bg-primary shadow-sm font-work-sans">
+		<header className="px-5 py-3 bg-secondary shadow-sm font-work-sans">
 			<nav className="flex justify-between items-center">
 				<Link href="/">
 					<Image src="/logo.png" alt="logo" width={144} height={60} />
